b3e5dd1b3394f94ed1726800e14e7471d15afb71
Bill Xiong <william.xiong@duke.edu>
2018-07-11 23:26:18 -0400

    basic file server

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', function(req, res, next) {
+    //error, no file specified
+    if(!req.files || !req.files.upload_file) {
+        res.status(400).send("Error upload file: Bad request.");
+        return;
+    }
+
+    let uploaded_file = req.files.upload_file;
+    let file_name = uploaded_file.name;
+
+    //change location if needed
+    uploaded_file.mv('./' + file_name, function(err) {
+        if(err) {
+            res.status(500).send(err);
+            return;
+        }
+        res.status(200).send("File " + file_name + " uploaded."); 
+    });
+});
+
+module.exports = router;
